ab95d0c95c09ae5d57ae7ea353bbb13100f2ec2b
zainab.hersi18@gmail.com <zainab.hersi18@gmail.com>
2025-02-12 14:12:12 +0000

    new file:   templates/add_movie.html 	new file:   templates/index.html 	new file:   templates/list_movies.html 	new file:   templates/movies.html 	new file:   templates/search_movie.html 	modified:   web.py

--- web.py
+++ web.py
@@ -1,11 +1,31 @@
-from flask import Flask
-
+from flask import * 
 
 app = Flask("myFlaskApp")
+app.secret_key = "secret"
 
+@app.route("/")  
+def index():
+    return render_template('index.html')
 
-@app.route("/sayHello")
-def greet():
-    return "<h3> Hello from Flask APP - looks like we are connected ;)</h3> <hr/>"
+@app.route("/add_movie", methods=["GET", "POST"])
+def add_movie():
+    if request.method == "POST":
+        session["Movie_ID"] = request.form.get("movie_id")
+        session["Movie_Name"] = request.form.get("movie_name")
+        session["Description"] = request.form.get("description")
+        return render_template("movies.html", movie=session)
+    return render_template("add_movie.html")
 
-app.run(debug=True)
+@app.route("/search_movie", methods=["GET", "POST"])
+def search_movie():
+    if request.method == "POST":
+        movie_id = request.form.get("movie_id")
+        return render_template("movies.html", movie_id=movie_id)
+    return render_template("search_movie.html")
+
+@app.route("/list_movies")
+def list_movies():
+    return render_template("list_movies.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
