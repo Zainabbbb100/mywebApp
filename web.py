ddad0bd41a7e15d67f82b5402b2c66a93cc31001
zainab.hersi18@gmail.com <zainab.hersi18@gmail.com>
2025-02-13 19:46:15 +0000

    modified:   templates/add_movie.html 	deleted:    templates/home.html 	modified:   templates/index.html 	modified:   templates/movie_detail.html 	modified:   web.py

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ import MySQLdb
 app = Flask(__name__)
 app.secret_key = "secret"
 
-#creates a  database connection
+# Creates a database connection
 def get_db_connection():
     return MySQLdb.connect(
         host="192.168.1.223",  
@@ -13,66 +13,15 @@ def get_db_connection():
         db="movies_db",  
         port=3306  
     )
-# def get_db_connection():
-#    return MySQLdb.connect(
-#        host="localhost",  
-#        user="root",  
-#        passwd="password", 
-#        db="movies_db",  
-#        port=3306  
-#     )
 
-
-#  Login Page
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        db = get_db_connection()
-        with db.cursor() as cursor:
-            sql = "SELECT * FROM users WHERE username = %s AND password = %s"
-            cursor.execute(sql, (username, password))
-            user = cursor.fetchone()
-        db.close()
-
-        if user:
-            session["user"] = username
-            return redirect(url_for("home"))  # Redirect to home after login
-        else:
-            return "Invalid username or password. <a href='/'>Try Again</a>"
-
+# Home Page (Main Landing Page)
+@app.route("/")
+def index():
     return render_template("index.html")
-
-#  Home Page (Redirects to login if not logged in)
-@app.route("/home")
-def home():
-    if "user" not in session:
-        return redirect(url_for("login"))
-    return render_template("home.html")
-
-#  Registration Route
-@app.route("/register", methods=["POST"])
-def register():
-    new_username = request.form.get("new_username")
-    new_password = request.form.get("new_password")
-
-    db = get_db_connection()
-    with db.cursor() as cursor:
-        sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
-        cursor.execute(sql, (new_username, new_password))
-        db.commit()
-    db.close()
-
-    return "Account created! <a href='/'>Go to Login</a>"
 
 # Route to Add a Movie
 @app.route("/add_movie", methods=["GET", "POST"])
 def add_movie():
-    if "user" not in session:
-        return redirect(url_for("login"))
-
     if request.method == "POST":
         movie_name = request.form.get("movie_name")
         description = request.form.get("description")
@@ -88,7 +37,7 @@ def add_movie():
 
     return render_template("add_movie.html")
 
-#  Search Movie Route
+# Search Movie Route
 @app.route("/search_movie", methods=["GET", "POST"])
 def search_movie():
     if request.method == "POST":
@@ -108,9 +57,6 @@ def search_movie():
 # List All Movies Route
 @app.route("/list_movies")
 def list_movies():
-    if "user" not in session:
-        return redirect(url_for("login"))
-
     db = get_db_connection()
     with db.cursor() as cursor:
         cursor.execute("SELECT * FROM movies")
@@ -118,12 +64,6 @@ def list_movies():
     db.close()
 
     return render_template("list_movies.html", movies=movies)
-
-#  Logout Route
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
